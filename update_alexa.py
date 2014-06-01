1b2ad25d92ca2acc6093268fa3ab4037e27cd21e
Andrew <a.tsonchev@gmail.com>
2014-06-01 18:47:24 +0100

    using glob to remove old CSVs

--- update_alexa.py
+++ update_alexa.py
@@ -4,22 +4,33 @@ import urllib
 import os
 import zipfile
 import datetime
+import glob
+
 
 def main():
     today = datetime.date.today()
     yesterday = today - datetime.timedelta(1)
-    if os.path.exists('top-1m_{0}.csv'.format(yesterday)):
-        os.remove('top-1m_{0}.csv'.format(yesterday))
-    if os.path.exists('top-1m_{0}.csv'.format(today)):
-        os.remove('top-1m_{0}.csv'.format(today))
-
+    # old way of removing file without glob:
+    # if os.path.exists('top-1m_{0}.csv'.format(yesterday)):
+    #     os.remove('top-1m_{0}.csv'.format(yesterday))
+    # if os.path.exists('top-1m_{0}.csv'.format(today)):
+    #     os.remove('top-1m_{0}.csv'.format(today))
+   
     urllib.urlretrieve('http://s3.amazonaws.com/alexa-static/top-1m.csv.zip', 
 		       'new_top-1m.csv.zip') 
     with zipfile.ZipFile('new_top-1m.csv.zip', 'r') as zip:
         zip.extractall('.')
-        os.rename('top-1m.csv', 
-                  'top-1m_{0}.csv'.format(today))        
+        # check for existing today file
+    if os.path.exists('top-1m_{0}.csv'.format(today)):
+        os.remove('top-1m_{0}.csv'.format(today))
+    os.rename('top-1m.csv', 
+              'top-1m_{0}.csv'.format(today))        
     os.remove('new_top-1m.csv.zip')
+    # remove any old files after the new one has been downloaded 
+    file_list = glob.glob('top-1m_*.csv')
+    for file in file_list:
+        if file != 'top-1m_{0}.csv'.format(today):
+            os.remove(file)
 
 if __name__ == '__main__':
     main()
