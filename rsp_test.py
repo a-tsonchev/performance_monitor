8bcc65f633b3578bfec857ba75990c1e29b560ee
Andrew <a.tsonchev@gmail.com>
2014-06-01 13:25:43 -0400

    moditied test

--- rsp_test.py
+++ rsp_test.py
@@ -1,5 +1,6 @@
 '''Module holding the main methods for performing response time testing.'''
 
+import grequests
 import requests
 import csv
 import datetime
@@ -8,13 +9,24 @@ import pymysql
 
 def main():
     today = datetime.date.today()
-    with open('/root/ddos_detection/top-1m_{date}.csv'
-              .format(today), 'r') as csvfile:
-
-        alexa = csv.reader(csvfile)
-        for rank, host in alexa:
-            rsp = requests.head('http://' + host)
-	    print host, rsp.elapsed
+    with open('/root/ddos_detection/top-1m_{0}.csv'
+         .format(today), 'r') as csvfile:
+	
+        raw = csv.reader(csvfile)
+	alexa = [(rank, 'http://' + host) for rank, host in raw]
+	alexa_chunks = []
+	for x in range(0, len(alexa), 10):
+   	    req = [grequests.head(u[1], timeout=1, allow_redirects=False) for u in alexa[x:x+10]]
+            rsp = grequests.map(req)
+	    durations = []
+	    for a in rsp:
+		try:
+		    durations.append((a.url, a.elapsed))
+		except:
+		    pass
+			
+	    print durations
+          
             
 
 
